61be6bf9e73c572868dd152b4c639a846f1526e8
Sir-Baconn <smattia13579@gmail.com>
2017-05-24 22:58:25 -0400

    gui

--- gui.py
+++ gui.py
@@ -9,6 +9,8 @@ class Example(wx.Frame):
         self.InitUI()
 
     def InitUI(self):
+        panel = wx.Panel(self, -1)
+
         menubar = wx.MenuBar()
         fileMenu = wx.Menu()
         quitMI = wx.MenuItem(fileMenu, APP_EXIT, '&Quit\tCtrl+Q')
@@ -29,6 +31,12 @@ class Example(wx.Frame):
         fileMenu.Append(quitMI)
 
         menubar.Append(fileMenu, '&File')
+
+        txtCtrl = wx.TextCtrl(panel, pos=(50,50), size=(100,20))
+        btn = wx.Button(panel, label='Search', pos=(150, 48))
+        
+        btn.Bind(wx.EVT_BUTTON, self.PrintText)
+
         self.SetMenuBar(menubar)
 
         self.Bind(wx.EVT_MENU, self.OnQuit, id=APP_EXIT)
@@ -36,6 +44,9 @@ class Example(wx.Frame):
         self.SetSize((400, 400))
         self.Center()
         self.Show()
+
+    def PrintText(self, e):
+        print(txtCtrl.GetValue())
 
     def OnQuit(self, e):
         self.Close()
